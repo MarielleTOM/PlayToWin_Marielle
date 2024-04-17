76dccdbb077ab5fc516613cfd9bb3f54e6ddee0a
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2024-04-17 17:31:55 -0300

    17/04-2

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,12 @@
 const {Sequelize} = require("sequelize");
 
+
 const sequelize = new Sequelize(
     process.env.DB_DATABASE,
     process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
+    
     {
+        password: "postgresql",
         host: process.env.DB_HOST, 
         port: process.env.DB_PORT, 
         dialect: process.env.DB_DIALECT,
--- index.js
+++ index.js
@@ -1,72 +1,43 @@
 require("dotenv").config();
 const conn = require("./db/conn");
-
-const Usuario = require('./models/Usuario');
-const Jogo = require('./models/Jogo');
-
-const express = require('express');
+const Usuario = require("./models/Usuario");
+const express = require("express");
 const app = express();
 
-app.use(
-    express.urlencoded({
-        extended:true,
-    })
-);
+// Middleware para analisar o corpo da solicitação
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("usuario/novo", (req,res)=>{
-    res.sendFile(`${__dirname}/views/formUsuario.html`)
+app.get("/usuarios/novo", (req, res) => {
+    res.sendFile(`${__dirname}/views/formUsuario.html`);
 });
 
-app.post("/usuario/novo", async (req,res)=>{
-    const nickname = req.body.nickname;
-    const nome = req.body.nome;
+app.post("/usuarios/novo", async (req, res) => {
+    try {
+        const { nickname, nome } = req.body;
+        
+        const dadosUsuario = {
+            nickname,
+            nome,
+        };
+        const usuario = await Usuario.create(dadosUsuario);
 
-    const dadosUsuario={
-        nickname,
-        nome,
-    };
-
-    const usuario = await Usuario.create(dadosUsuario);
-
-    res.send('Usuário inserido sob o id' + usuario.id);
+        res.send("Usuário inserido sob o id " + usuario.id);
+    } catch (error) {
+        console.error("Erro ao inserir usuário:", error);
+        res.status(500).send("Erro ao inserir usuário");
+    }
 });
 
+app.listen(8000, () => {
+    console.log("Servidor está ouvindo na porta 8000");
+});
 
-app.use(
-    express.urlencoded({
-        extended:true,
+conn
+    .sync()
+    .then(() => {
+        console.log("Conectado e sincronizado ao banco de dados com sucesso!");
     })
-);
-app.use(express.json());
-
-app.get("usuario/novo", (req,res)=>{
-    res.sendFile(`${__dirname}/views/formUsuario.html`)
-});
-
-
-app.post("/jogo/novo", async (req,res)=>{
-    const titulo = req.body.titulo;
-    const descricao = req.body.descricao;
-    const precoBase = req.body.precoBase
-
-    const dadosUsuario={
-        titulo,
-        descricao,
-        precoBase,
-    };
-
-    const jogo = await Jogo.create(dadosJogo);
-
-    res.send('Jogo inserido sob o id' + jogo.id);
-});
-
-app.listen(8000 ,() =>{
-    console.log("Server rodando na porta 8000!")
-})
-
-conn.sync().then(()=>{
-    console.log('Conectado ao banco de dados com sucesso!');
-}).catch((err)=>{
-    console.log('Ocorre um erro: ' + err);
-});
+   .catch (err => {
+        console.error("Ocorreu um erro ao conectar/sincronizar o banco de dados:", err);
+    });
